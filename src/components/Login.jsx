cb5e3c43b5b551a3a22988ca285016458fc607bd
Pawan Kumar <pawankumar.nov5.com>
2024-10-02 19:37:57 +0530

    commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -37,7 +37,7 @@ export default function Login() {
         console.log('Logging in...');
 
         try {
-            const res = await api.post('https://practice-backend-lilac.vercel.app/account/login', loginData);              
+            const res = await api.post('account/login', loginData);              
             if (res.status === 200) {
                 setIsAuthenticated(true);
                 navigate('/');
