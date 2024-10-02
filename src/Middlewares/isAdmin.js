aef571dd05a0cf7bd718d755f7144be7a7686b0a
Pawan Kumar <pawankumar.nov5.com>
2024-10-02 19:39:01 +0530

    commit

--- src/Middlewares/isAdmin.js
+++ src/Middlewares/isAdmin.js
@@ -3,7 +3,7 @@ import api from '../backendCall';
 // Function to check if the user is an admin
 export const CheckIsAdmin = async () => {
     try {
-        const res = await api.get('https://practice-backend-lilac.vercel.app/account/isAdmin');
+        const res = await api.get('/account/isAdmin');
         if (res.status === 200) {
             return res.data.isAdmin;
         } else {
