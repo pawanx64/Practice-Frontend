093fb8a7c9f7d654a3c1f4567167dbe5ca453804
Pawan Kumar <pawankumar.nov5.com>
2024-10-02 19:55:05 +0530

    commit

--- src/Middlewares/isLoggedIn.js
+++ src/Middlewares/isLoggedIn.js
@@ -1,20 +1,17 @@
 import axios from 'axios';
 import api from '../backendCall';
-
+const token = localStorage.getItem('token');
 // Function to check if the user is an loggedin
 export const isLoggedIn = async () => {
     try {
         const res = await axios.get('https://practice-backend-lilac.vercel.app/account/isLoggedIn', {
-            withCredentials: true
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
         });
         return res.status === 200;
     } catch (error) {
-        if (error.response && error.response.status === 401) {
-            // Redirect to login or take appropriate action
-            console.error("User is not authorized, redirecting to login...");
-        } else {
-            console.error("Error checking login status:", error);
-        }
+        console.error("Error checking login status:", error);
         return false;
     }
 };
